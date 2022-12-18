0bea6178219060ec4af769191c425e94d0b9fddb
iThulin <ian.thulin@icloud.com>
2022-12-17 18:23:27 -0700

    Struggling ith file structure

--- Day 1.py
+++ Day 1.py
@@ -1,3 +1,23 @@
-data = open('day1.txt', 'r')
-print(data.read())
+file = open('day1.txt', 'r')
+#print(data.read())
 
+#elves = []
+elf_number = 0
+
+data = file.read()
+elves = data.split()
+
+file.close()
+"""
+with open('day1.txt', 'r') as data:
+    for line in data:
+        #print(line)
+        if line.strip():
+            #elves[elf_number].append(list)
+            print(line)
+        else: 
+            print("Line break")
+            #elf_number += 1
+            continue
+"""
+print(elves)
