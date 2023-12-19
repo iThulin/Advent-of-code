192d23189c35f08a138f69b521e2111f726df573
Ian <ian.thulin@icloud.com>
2023-12-19 10:06:16 -0700

    Fixed bug in data handler and added .env for tokens

--- Pull data.py
+++ Pull data.py
@@ -1,52 +1,33 @@
 from aocd import get_data
 import os
 
-os.environ["AOC_SESSION"] = '53616c7465645f5f34f19f0a2e183853db7a905f9408a576b69bd803d00a46dce7cd9fcc857e79661ee08f33deb38043a666390fe6682180d86c316c6c3d241a'
 token = os.getenv('AOC_SESSION')
-#print(token)
-
-
-
 
 fetch_date = 1
 fetch_year = 2023
 
-def make_data_file(fetch_date):
-
+def make_data_file(fetch_date, path):
     data = get_data(day=fetch_date, year=2023)
-
-    filename = path + 'Day_' + str(fetch_date) + ".txt"
+    filename = path + "/data.txt"
 
     with open(filename, 'w')as output:
         output.write(data)
 
+# \fetch_year exists?
 if os.path.isdir(str(fetch_year)):
     parent_dir = str(fetch_year)
-    path = parent_dir + '/' + str(fetch_date)
+    path = parent_dir + '/Day_' + str(fetch_date)
+    # \fetch_year\fetch_date exists?
     if os.path.isdir(path):
-        # make file
-        make_data_file(fetch_date)
+        make_data_file(fetch_date, path)
     else:
         os.mkdir(path)
-        #make file
-        path = path + 'Day_' + str(fetch_date)
-        make_data_file(fetch_date)
+        #path = path + '/Day_' + str(fetch_date)
+        make_data_file(fetch_date, path)
 else:
     os.mkdir(str(fetch_year))
     parent_dir = str(fetch_year)
-    path = parent_dir + '/' + str(fetch_date)
+    path = parent_dir + '/Day_' + str(fetch_date)
     os.mkdir(path)
-
-
-
-'''
-
-directory = str(fetch_year) +  '/Day' + str(fetch_date)
-parent_dir = 'C:/Users/user/Advent-of-code/Advent-of-code'
-
-path  = os.path.join(parent_dir, directory)
-
-os.mkdir(path)
-
-
-'''
+    path = path + '/Day_' + str(fetch_date)
+    make_data_file(fetch_date, path)
