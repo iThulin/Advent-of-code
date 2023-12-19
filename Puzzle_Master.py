31158ed6f15be654cf6c0b1bed8f5c086eeabde3
Ian <ian.thulin@icloud.com>
2023-12-19 10:20:23 -0700

    Added master file for puzzle solutions

---
+++ Puzzle_Master.py
@@ -0,0 +1,15 @@
+from aocd import submit
+
+# Parameters
+Year = 2023
+Day = 1
+my_answer = ''
+
+# Open & Manipulate Data
+with open('data.txt') as data_file:
+    print(data_file.read())
+
+
+
+# Submit answer
+#submit(my_answer, part="a", day=Day, year=Year)
