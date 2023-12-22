1ec0a2e853aad1be6808fc8b2b7bc3f8649036f5
Ian <ian.thulin@icloud.com>
2023-12-21 17:08:57 -0700

    Progress save for Day 3-1

--- 2023/Day_3/Puzzle_3.py
+++ 2023/Day_3/Puzzle_3.py
@@ -1,4 +1,5 @@
 from aocd import submit
+import re
 
 # Parameters
 Year = 2023
@@ -11,6 +12,83 @@ with open(path) as data_file:
     input_a = data.splitlines()
     input_b = input_a
 
+# Helper Functions
+def is_symbol(text):
+    return bool(re.match(r'^[\W_]+$', text))
+
+def search_neighbors(array, row, col):
+    neighbors = []
+    domain_x = len(array)
+    domain_y = len(array[0])
+
+    directions = [(-1, -1), (-1, 0), (-1, 1),
+                  (0, -1),(0, 1),
+                  (1, -1), (1, 0), (1, 1)]
+
+    for dir_r, dir_c in directions:
+        new_row, new_col = row + dir_r, col + dir_c
+
+        # Check if the new address is in the domain
+        if 0 <= new_row < domain_x and 0 <= new_col < domain_y:
+            neighbors.append((array[new_row][new_col], new_row, new_col))
+
+    print(f"neighbors: {neighbors}")
+    return neighbors
+
+def trim_neighbors(neighbors):
+    trim_neighbors = []
+    print(f"Original: {neighbors}")
+    upper, middle, lower = neighbors[:3], neighbors[3:5], neighbors[5:]
+    print(f" upper: {upper}\n middle: {middle}\n lower: {lower}")
+
+    #I will brute force this interaction
+    if upper[0][0].isnumeric():
+        trim_neighbors.append(upper[0])
+    if upper[1][0].isnumeric() and upper[0] not in trim_neighbors:
+        trim_neighbors.append(upper[1])
+    if upper[2][0].isnumeric() and upper[1] not in trim_neighbors:
+        if upper[1][0].isnumeric() == False:
+            trim_neighbors.append(upper[2])
+
+    trim_neighbors.append(middle[0])
+    trim_neighbors.append(middle[1])
+
+    if lower[0][0].isnumeric():
+        trim_neighbors.append(lower[0])
+    if lower[1][0].isnumeric() and lower[0] not in trim_neighbors:
+        trim_neighbors.append(lower[1])
+    if lower[2][0].isnumeric() and lower[1] not in trim_neighbors:
+        if lower[1][0].isnumeric() == False:
+            trim_neighbors.append(lower[2])
+
+    print(f"Trimmed: {trim_neighbors}")
+    return trim_neighbors
+
+
+def scan_neighbors(array, row, col):
+    num_list = []
+
+    domain_x = len(array[0])
+
+    pointer = col
+
+    # Scan to the start of a number
+
+    while array[row][pointer - 1].isnumeric() and 0 <= pointer - 1:
+        pointer -= 1
+
+    while array[row][pointer].isnumeric():
+        num_list.append(array[row][pointer])
+        if pointer + 1 < domain_x:
+            pointer += 1
+        else:
+            break
+
+    print(num_list)
+
+    return sum(map(int, num_list))
+    
+
 # --- Part One ---
 def part_a(input_a):
  
@@ -21,6 +99,7 @@ def part_a(input_a):
     # 4. Add number to the sum and continue search
 
     array = []
+    part_sum = 0
 
     for line in input_a:
         row = []
@@ -31,9 +110,27 @@ def part_a(input_a):
                 row.append(character)
         array.append(row)
 
-    for i in range(len(array)):
-        print(array[i])
+    for row_num, row in enumerate(array):
+        #print(row)
+        for index, string_val in enumerate(array[row_num]):
+            # 1. Find a symbol
+            if is_symbol(string_val):
+                print(f"{string_val} at Row, index {row_num},{index}")
+                # 2. Look for a digit in surrounding directions.
+                neighbors = search_neighbors(array, row=row_num, col=index)
+                trimmed_neighbors = trim_neighbors(neighbors)
+                # 3. If number is found, scan horizontally for full number
+                for j, item in enumerate(trimmed_neighbors):
+                    print(f'found a number: {item}')
+                    part_num = scan_neighbors(array, row=neighbors[j][1] , col=neighbors[j][2] )
+                    part_sum += part_num
+                    print(f"Adding {part_num} to make sum= {part_sum}\n")
+            else:
+                #print(f"Row, Index, String: {row_num}, {index}, {string_val}")
+                pass
 
+
+    answer_a = part_sum
 
     return answer_a
 
@@ -41,13 +138,14 @@ def part_a(input_a):
 
 def part_b(input_b):
 
+    answer_b = 'Still learning'
     return answer_b
 
 # --- SUBMIT ANSWERS ---
 
 answer_a = part_a(input_a)
 print(f"Answer_a: {answer_a}")
-#submit(my_answer_a, part="a", day=Day, year=Year)
+#submit(answer_a, part="a", day=Day, year=Year)
 
 answer_b = part_b(input_b)
 print(f"Answer_b: {answer_b}")
